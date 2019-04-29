9c810f9993707a13c069f2fd6db280648df4053f
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-04-29 17:12:48 -0400

    Add SearchBar Component class, return, render

--- instagram/instagram-app/src/App.js
+++ instagram/instagram-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import instaLogo from './instaLogo.jpg';
 import './App.css';
 
 import dummyData from './dummy-data';
@@ -7,25 +7,21 @@ import SearchBar from './components/SearchBar';
 
 
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = '';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  )
+  render() {
+
+    <SearchBar />,
+    <PostContainer />,
+    <CommentSection />
+  }   
+  }
 }
 
 export default App;
--- instagram/instagram-app/src/components/SearchBar.js
+++ instagram/instagram-app/src/components/SearchBar.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+class SearchBar extends React.Component {
+    state = {
+        query: '',
+    }
+
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        })
+    }
+
+    render() {
+        return (
+            <form>
+                <input>
+                placeholder="Search for..."
+                ref={input => this.search = input}
+                onChange={this.handleInputChange}
+                </input>
+                <p>{this.state.query}</p>
+            </form>
+        )
+    }
+}
 
 
 
